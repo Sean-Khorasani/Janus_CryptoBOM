63b3f630d557946345b7ed7c875b30469d0df264
Shahin <shahin@resket.ca>
2026-06-16 17:33:27 -0700

    ToastProvider and error boundary wired into root

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { I18nProvider } from "./i18n";
 import { ToastProvider } from "./hooks/useToast";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <I18nProvider>
-      <ToastProvider>
-        <App />
-      </ToastProvider>
-    </I18nProvider>
+    <ErrorBoundary label="Janus">
+      <I18nProvider>
+        <ToastProvider>
+          <App />
+        </ToastProvider>
+      </I18nProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
